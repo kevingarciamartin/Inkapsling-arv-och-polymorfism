f6a93704d780672531a99b649033bb7532a70a8f
Kevin Garcia Martin <kevingm@live.se>
2024-05-29 10:45:31 +0200

    Add project files.

---
+++ Inkapsling, arv och polymorfism/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Inkapsling__arv_och_polymorfism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person person = new("Kevin", "Garcia Martin");
+                person.setAge(28);
+            }
+            catch (ArgumentException aex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(aex.Message);
+            }
+            finally
+            {
+                Console.ResetColor();
+            }
+        }
+    }
+}
