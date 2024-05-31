b90023678ce5a6e52dbb15c608f7a64eaadcf6ef
Kevin Garcia Martin <kevingm@live.se>
2024-05-31 10:14:48 +0200

    Add UserError class library

---
+++ UserError/TextInputError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inkapsling__arv_och_polymorfism.Polymorfism
+{
+    public class TextInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a text input in a numeric only field. This fired an error!";
+        }
+    }
+}
---
+++ UserError/WhiteSpaceInputError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inkapsling__arv_och_polymorfism.Polymorfism
+{
+    public class WhiteSpaceInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use white space as your only input. This fired an error!";
+        }
+    }
+}
